434af5ec230e65e2d7d2d1800584bc3333f84b23
Moh Bareche <mbareche.bic@gmail.com>
2019-03-15 14:12:30 -0400

    server-setup

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const config = require('./config/database');
+const path = require('path');
+
+mongoose.Promise = global.Promise;
+mongoose.connect(config.uri, {
+    useNewUrlParser: true
+}, (err) => {
+    if (err) {
+        console.log('Ne peut se connecter à la base de données:', err);
+    } else {
+        // console.log(config.secret);
+        console.log('Connecté à la base de données: ' + config.db);
+    }
+});
+
+app.use(express.static(__dirname + '/client/dist/'));
+
+
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname + '/client/dist/index.html'));
+});
+
+app.listen(8080, () => {
+    console.log('Listening on port 8080...');
+});
